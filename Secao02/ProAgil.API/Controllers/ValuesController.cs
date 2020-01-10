4e64f88f8fac0530c7b56438ab32c76f9112c12a
Amauri Junior <amauri.junior.1989@gmail.com>
2020-01-10 08:55:04 -0300

    Aprendendo a Trabalhar com EF CORE

---
+++ Secao02/ProAgil.API/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.API.Data;
+using ProAgil.API.Models;
+
+namespace ProAgil.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public DataContext Context { get; }
+
+        public ValuesController(DataContext context)
+        {
+            Context = context;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await Context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco De Dados Falhou!");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Evento>> Get(int id)
+        {
+            return await Context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+        }
+    }
+}
